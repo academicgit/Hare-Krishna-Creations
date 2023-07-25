85510fb934affc693593de353af569b9a9379b0a
Johnathon Frederick <academics@mailfence.com>
2023-07-25 20:54:16 +0530

    Added Qr Code

---
+++ main.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+const copyrightElement = document.querySelector('#copyright');
+copyrightElement.innerHTML = `&copy; ${currentYear} Hare Krishna Creations. All rights reserved.`;
